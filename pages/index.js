c35db8edaf2c5355fcecd807dea554078ca8de97
gazdagergo <info@gazdagergo.com>
2024-06-13 10:40:35 +0200

    mouse move as useState - renders a lot

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [isGoodby, setGoodby] = useState(false)
+  const [mouseP2, setM] = useState(0)
+
+  const mosuePosition = useRef(0)
+
   const handleClick = () => {
     setGoodby(prevGoodby => !prevGoodby)
   }
 
+  const handleMouseMove = event => {
+    mosuePosition.current = event.clientX
+    setM(event.clientX)
+  }
+
   return (
-    <main>
+    <main onMouseMove={handleMouseMove}>
       <h1>Greeting</h1>
       <p>{isGoodby? 'goodby': 'hello'}</p>
-
+      {/*<p>mouse postion: {mosuePosition.current}</p>*/}
+      <p>{mouseP2}</p>
       <button onClick={handleClick}>Toggle</button>
     </main>
   );
