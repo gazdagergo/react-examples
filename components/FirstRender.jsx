b14584c1efa48a6669140d90b70051abbd57eff7
gazdagergo <info@gazdagergo.com>
2024-06-20 19:24:24 +0200

    order of mount and render

--- components/FirstRender.jsx
+++ components/FirstRender.jsx
@@ -1,24 +1,34 @@
-import {useEffect} from "react";
+import { useEffect, useRef } from "react";
 
 const ChildComp = () => {
+  const divRef = useRef(null)
+
   useEffect(() => {
     console.log('child mounted')
   }, []);
 
   return (
-    <div>I'm a child {console.log('child render')}</div>
+    <div ref={el => {
+      console.log('child div ref set')
+      divRef.current = el
+      }}>
+      I'm a child {console.log('child render')}
+    </div>
   )
 }
 
 const FirstRender = () => {
-
+  const divRef = useRef(null)
 
   useEffect(() => {
     console.log('parent mounted')
   }, []);
 
   return (
-    <div>
+    <div ref={el => {
+      console.log('parent div ref set')
+      divRef.current = el
+    }}>
       <div>first render {console.log('parent render')}</div>
       <ChildComp />
     </div>
